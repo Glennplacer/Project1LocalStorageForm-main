69c52edf7e2676aeeef07fb8c39533fee939457a
Glenn Anrei Placer <glennplacer1@gmail.com>
2021-04-25 21:54:04 +0800

    Adding elements into Local Storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,6 @@ function eventListeners() {
     tweetList.addEventListener('click', removeTweet);
 }
 
-
 //functions
 
 function newTweet(e) {
@@ -37,6 +36,9 @@ function newTweet(e) {
 
     //add to the list
     tweetList.appendChild(li);
+
+    //add to local storage
+    addTweetLocalStorage(tweet);
 }
 
 //remove the tweets from the DOM
@@ -44,4 +46,27 @@ function removeTweet(e) {
     if(e.target.classList.contains('remove-tweet')) {
         e.target.parentElement.remove();
     } 
+}
+
+//adds the tweets into the local storage
+function addTweetLocalStorage(tweet) {
+    let tweets = getTweetsFromStorage();
+
+    //add the tweet into the array
+    tweets.push(tweet);
+
+    //convert tweet array into string
+    localStorage.setItem('tweets', JSON.stringify(tweets) );
+}
+
+function getTweetsFromStorage() {
+    let tweets;
+    const tweetsLS = localStorage.getItem('tweets');
+    //get the values, if null is returned then we create an empty array
+    if(tweetsLS === null) {
+        tweets = [];
+    } else {
+        tweets = JSON.parse( tweetsLS );
+    }
+    return tweets;
 }
